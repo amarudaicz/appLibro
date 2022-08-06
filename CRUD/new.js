793bdc16b070830037641932a65742671473340c
amaru <amarudaicz10gmail.com>
2022-08-05 21:03:41 -0300

    commit

--- CRUD/new.js
+++ CRUD/new.js
@@ -4,13 +4,10 @@ function add(title, author, genere, year, cost, price) {
     
     let listado = fs.readFileSync("./data.json", "utf-8");
     let listadoPar = JSON.parse(listado);
-    id=1
-
-    listadoPar.forEach(element => {
-        id+= 1
-    });
-
-    let libro = {title, author, genere, year, cost, price, id };
+    generateId = () => parseInt(Math.random() * 99999) ;
+    id = generateId()
+    let libro = {title, author, genere, year, cost, price, id};
+    
     listadoPar.push(libro);
     
     fs.writeFileSync("./data.json", JSON.stringify(listadoPar, null, 2));
